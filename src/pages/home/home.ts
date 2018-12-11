1ceee56588ab06c5872ce56992ea66fab63c005a
ramon morcillo <ramonmorcillocascales@gmail.com>
2018-12-11 21:39:32 +0100

    basic routing from home to other pages

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+//pages
+import { CreditsPage, GamePage, ProfilePage, RulesPage } from "../index.pages";
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  gamePage = GamePage;
+  creditsPage = CreditsPage;
+  profilePage = ProfilePage;
+  rulesPage = RulesPage;
   constructor(public navCtrl: NavController) {
 
   }
