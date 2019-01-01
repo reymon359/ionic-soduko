e901af598740c8e5a95b766bd5c4fc532ed1de2f
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-01-01 23:49:29 +0100

    Improving and cleaning code

--- src/pages/game/game.ts
+++ src/pages/game/game.ts
@@ -11,17 +11,6 @@ import { Game } from "../../models/index.models";
   templateUrl: 'game.html',
 })
 export class GamePage {
-  // boardBase: any[] = [
-  //   [2, 8, 6, 7, 5, 4, 9, 3, 1],
-  //   [9, 3, 1, 2, 8, 6, 7, 5, 4],
-  //   [7, 5, 4, 9, 1, 3, 8, 6, 2],
-  //   [8, 9, 2, 6, 7, 5, 4, 1, 3],
-  //   [6, 7, 5, 4, 3, 1, 2, 9, 8],
-  //   [4, 1, 3, 8, 9, 2, 6, 7, 5],
-  //   [5, 6, 9, 3, 4, 8, 1, 2, 7],
-  //   [3, 4, 7, 1, 2, 9, 5, 8, 6],
-  //   [1, 2, 8, 5, 6, 7, 3, 4, 9]
-  // ];
   public game: Game = {
     token: "",
     state: "",
@@ -42,9 +31,7 @@ export class GamePage {
   constructor(public navCtrl: NavController, public navParams: NavParams, private gameProv: GameProvider,
     private usefulProv: UsefulProvider, public alertCtrl: AlertController) {
     console.log("enters gamepage")
-    this.numbers = Array(10).fill(1).map((x, i) => i); // [0,1,2,3,4]
-    // this.numbers = Array(5).fill(4); // [4,4,4,4,4]
-    console.log(this.numbers);
+    this.numbers = Array(10).fill(1).map((x, i) => i);
     this.createNewGame();
   }
 
